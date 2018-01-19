e48116b375b61cfc3f354aa0a1dcd8e2430ff20b
isabel <isabel.youthboard@futureindesign.org>
2018-01-19 12:01:11 -0700

    added to hole 3

---
+++ hole_3.rb
@@ -0,0 +1,20 @@
+# For numbers 1-100 print “FIZZ” if the number is divisible by 3, 
+# “BUZZ” if the number is divisible by 5, “FIZZBUZZ” if the number 
+# is divisible by both 3 and 5 otherwise print the number.
+
+def fuzz
+    puts '1-100'
+    @inp = gets.to_f
+    case @inp
+        when @inp / 3 % 0
+            puts "FIZZ"
+        when @inp / 5 % 0 
+            puts "BUZZ"
+        when @inp / 3 % 0 && @inp / 5 % 0
+            puts "FIZZBUZZ"
+        else
+            puts @inp
+    end
+end
+
+fuzz
