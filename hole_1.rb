acd785d06e5f73c77f1895b477099eec63c8098a
isabel <isabel.youthboard@futureindesign.org>
2018-01-19 11:08:06 -0700

    starting on hole 2

---
+++ hole_1.rb
@@ -0,0 +1,15 @@
+arr = []
+
+
+# Create a method that takes in an array sums the array then returns the array and the sum
+# 
+
+def array()
+    puts 'Insert numbers'
+    arr = gets.to_f
+    arr.each do |a|
+        a.count
+    end
+
+end
+array
